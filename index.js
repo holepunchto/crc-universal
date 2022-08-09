c861d3766b7ad4f39584639f8888a907cfbfc886
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-09 11:21:03 +0200

    Fall back to JS version if native fails to load

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const native = require('crc-native')
 const browser = require('./browser')
 
-exports.crc32 = function crc32 (buffer) {
-  return buffer.byteLength < 24 ? browser.crc32(buffer) : native.crc32(buffer)
+try {
+  const native = require('crc-native')
+
+  exports.crc32 = function crc32 (buffer) {
+    return buffer.byteLength < 24 ? browser.crc32(buffer) : native.crc32(buffer)
+  }
+} catch {
+  module.exports = browser
 }

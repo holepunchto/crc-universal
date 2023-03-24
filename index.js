84eecd4bc42f900ba787cb1fe1b4c47826ec18da
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-24 15:21:15 +0100

    Use `crc-native` from npm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fallback = require('./fallback')
 
 try {
-  const native = require('./native')
+  const native = require('crc-native')
 
   exports.crc32 = function crc32 (buffer) {
     return buffer.byteLength <= 24 ? fallback.crc32(buffer) : native.crc32(buffer)

137778a874bdd82a383977646355f035a03c9136
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-06 08:56:48 +0200

    Migrate to Prettier (#2)

--- test.mjs
+++ test.mjs
@@ -3,5 +3,12 @@ import test from 'brittle'
 import { crc32 } from './fallback.js'
 
 test('crc32', (t) => {
-  t.is(crc32(Buffer.from('abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh')), 0x6b64f5d)
+  t.is(
+    crc32(
+      Buffer.from(
+        'abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh'
+      )
+    ),
+    0x6b64f5d
+  )
 })
